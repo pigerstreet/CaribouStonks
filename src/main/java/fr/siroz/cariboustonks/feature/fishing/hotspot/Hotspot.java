c321fea4cafac417a78c197689d7c288d8e68f3d
pigerstreet <jj123jonathan@gmail.com>
2026-01-19 13:37:39 +1100

    Delete src/main/java/fr/siroz/cariboustonks/feature/fishing directory

--- src/main/java/fr/siroz/cariboustonks/feature/fishing/hotspot/Hotspot.java
+++
@@ -1,9 +0,0 @@
-package fr.siroz.cariboustonks.feature.fishing.hotspot;
-
-import net.minecraft.world.entity.decoration.ArmorStand;
-import net.minecraft.world.phys.Vec3;
-
-import java.util.Optional;
-
-record Hotspot(ArmorStand entity, Vec3 centerPos, Optional<String> perk) {
-}
